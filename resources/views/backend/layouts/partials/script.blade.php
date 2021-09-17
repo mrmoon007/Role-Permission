5809c552163c22d42b13089bf176a948a8631680
mrmoon007 <mrmoon025@gmail.com>
2021-09-17 23:24:19 +0600

    index page mastering

---
+++ resources/views/backend/layouts/partials/script.blade.php
@@ -0,0 +1,17 @@
+    <script src="{{ asset('backend/assets/lib/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/popper.js/popper.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/bootstrap/bootstrap.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/jquery-ui/jquery-ui.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/jquery.sparkline.bower/jquery.sparkline.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/d3/d3.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/rickshaw/rickshaw.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/chart.js/Chart.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/Flot/jquery.flot.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/Flot/jquery.flot.pie.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/Flot/jquery.flot.resize.js') }}"></script>
+    <script src="{{ asset('backend/assets/lib/flot-spline/jquery.flot.spline.js') }}"></script>
+
+    <script src="{{ asset('backend/assets/js/starlight.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/ResizeSensor.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/dashboard.js') }}"></script>
